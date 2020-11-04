2e183af7bba37430185ec237311934feac79b117
GOKOL Semi <gokol.semih@gmail.com>
2020-11-04 16:17:18 +0100

    Creation, connexion et remplissage de la bdd

---
+++ schema.sql
@@ -0,0 +1,48 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS game;
+
+CREATE TABLE user (
+  idUser INTEGER PRIMARY KEY,
+  username TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL
+
+);
+
+CREATE TABLE game (
+  idGame INTEGER PRIMARY KEY,
+  gameName TEXT NOT NULL,
+  plateform TEXT NOT NULL
+);
+
+CREATE TABLE userGame (
+  idUserGame INTEGER PRIMARY KEY,
+  fkUser INTEGER REFERENCES user(idUser),
+  fkGame INTEGER REFERENCES game(idGame)
+);
+
+INSERT INTO game (gameName, plateform)
+  VALUES 
+  ('Rocket League', 'PC'),
+  ('Rocket League', 'XBOX'),
+  ('Rocket League', 'PlayStation'),
+  ('Rocket League', 'Switch'),
+  ('Fortnite', 'PC'),
+  ('Fortnite', 'XBOX'),
+  ('Fortnite', 'PlayStation'),
+  ('Fortnite', 'Switch'),
+  ('Diablo3', 'PC'),
+  ('Diablo3', 'XBOX'),
+  ('Diablo3', 'PlayStation'),
+  ('Diablo3', 'Switch'),
+  ('Civilization5', 'PC'),
+  ('Civilization5', 'XBOX'),
+  ('Civilization5', 'PlayStation'),
+  ('Civilization5', 'Switch'),
+  ('Rust', 'PC'),
+  ('Rust', 'XBOX'),
+  ('Rust', 'PlayStation'),
+  ('FootballManager2020', 'PC'),
+  ('FootballManager2020', 'XBOX'),
+  ('FootballManager2020', 'PlayStation'),
+  ('FootballManager2020', 'Switch');
+  
--- tpPython.py
+++ tpPython.py
@@ -1,6 +1,58 @@
 from flask import Flask
+import sqlite3
+
+import os
+import click
+from flask import current_app, g
+from flask.cli import with_appcontext
+
 app = Flask(__name__)
 
+app.secret_key = b'tartiflette69latrick'
+
+##Function for the connection to the database##
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            os.path.join(app.instance_path, 'tp.db'),
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory = sqlite3.Row
+
+    return g.db
+##----------------------------------------##
+
+##Function to close the database instance##
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+##----------------------------------------##
+
+#Functions to initialize the database
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
+##----------------------------------------##
+
+
+
+####Main functions for the app####
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+####----------------------------------####
+
+app.teardown_appcontext(close_db)
+app.cli.add_command(init_db_command)
