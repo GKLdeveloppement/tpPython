0ce5746950b90c9de5f42a9c8a4a8491c20a29e1
GOKOL Semi <gokol.semih@gmail.com>
2020-11-19 10:49:17 +0100

    Ajout requete d'insert d'un jeu choisi dans la liste déroulante (/addG), il faut faire la partie session pour recupérer le user connecté

--- tpPython.py
+++ tpPython.py
@@ -8,6 +8,7 @@ from flask.cli import with_appcontext
 from flask import request
 import sys
 import traceback
+from flask import redirect, url_for
 
 app = Flask(__name__, static_folder="./templates")
 
@@ -62,10 +63,6 @@ def errorLog():
 ####Main functions for the app####
 
 
-
-####----------------------------------####
-
-##-------------Routage-------------##
 #Gérer les connexions et les sessions des utilisateurs
 @app.route('/login')
 def login():
@@ -93,9 +90,11 @@ def addGame():
     try:
         conn = get_db()
         gameList = conn.execute('SELECT * FROM game').fetchall()
+        ##uncomment to test what return the sqlite request
         # for i in gameList:
         #     for x in i:
         #         print(x)
+        ##
     except sqlite3.Error as error:
         errorLog()
     return render_template('addGame.html', gameList = gameList)
@@ -105,16 +104,19 @@ def addG():
     #need to check if the user is logged in?
     if request.method=="POST":
         if 'add-game' in request.form:
-            #userId = #getuserid
             try:
                 conn = get_db()
-                gameChoose = request.form
-                print(gameChoose)
-                conn.execute('INSERT INTO game (gameName, plateform) VALUES ("azertyuiop", "PS4")') #mettre variable
+                #get the variable in the option HTML element which contain the choise of the user
+                idGameChoose = request.form.get("gameList")
+                idUser = 1 ###Maxime need to get here the user id who is logged in
+                # print(idGameChoose)
+                #Stock the both variables in a table to use it in the request as params
+                reqParam = [idGameChoose, idUser]
+                conn.execute('INSERT INTO userGame (fkGame, fkUser) VALUES (?, ?)',reqParam) #mettre variable
                 conn.commit()
             except sqlite3.Error as error:
                 errorLog()
-    return render_template('addGame.html')
+    return redirect(url_for('addGame'))
 ####----------------------------------####
 
 app.teardown_appcontext(close_db)
